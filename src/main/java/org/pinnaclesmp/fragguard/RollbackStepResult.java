7a36eb0b50cf53d09f5919526fb26ff40e1b2815
Kevin <mccreeper1319@icloud.com>
2026-08-20 11:50:12 -0400

    Track rollback conflicts in batch results

--- src/main/java/org/pinnaclesmp/fragguard/RollbackStepResult.java
+++ src/main/java/org/pinnaclesmp/fragguard/RollbackStepResult.java
@@ -1,4 +1,7 @@
 package org.pinnaclesmp.fragguard;
 
-record RollbackStepResult(int sequence, boolean changed) {
+record RollbackStepResult(int sequence, boolean changed, boolean conflicted) {
+    RollbackStepResult(int sequence, boolean changed) {
+        this(sequence, changed, false);
+    }
 }
