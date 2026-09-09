3cecf057e3c3ae177757071c73d7d6c7aa5fff90
Kevin <mccreeper1319@icloud.com>
2026-09-08 21:10:01 -0400

    Bump FragGuard to 26.2-1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.pinnaclesmp"
-version = "26.2-1.1.0"
+version = "26.2-1.1.1"
 
 java {
     // Paper 26.x uses the newer Paper API versioning and currently documents Java 25 for 26.x builds.
