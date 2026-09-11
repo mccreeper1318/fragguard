008ab93d1e770926f1b2916d44ce556e75db6ea1
Kevin <261039506+mccreeper1318@users.noreply.github.com>
2026-09-10 20:25:10 -0400

    Start FragGuard 26.2-1.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.pinnaclesmp"
-version = "26.2-1.1.1"
+version = "26.2-1.1.2"
 
 java {
     // Paper 26.x uses the newer Paper API versioning and currently documents Java 25 for 26.x builds.
